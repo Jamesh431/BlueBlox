da61f3c112e0a3cc09a6534e89d61bd81fe74930
james <james@devpipeline.com>
2023-04-16 00:00:29 -0600

    category divs for landing page

---
+++ src/components/pages/Landing.js
@@ -0,0 +1,19 @@
+export default function Landing() {
+  <div className="LandingPage">
+    <div className="SortedShows">
+      <div className="Popular"></div>
+      <div className="Category"></div>
+      <div className="Category"></div>
+      <div className="Category"></div>
+      <div className="Category"></div>
+      <div className="Category"></div>
+      <div className="Category"></div>
+      <div className="Category"></div>
+      <div className="Category"></div>
+      <div className="Category"></div>
+      <div className="Category"></div>
+      <div className="Category"></div>
+      <div className="Category"></div>
+    </div>
+  </div>
+}
