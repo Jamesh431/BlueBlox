015933f0d53e9096649605d1283ef788851771f2
james <james@devpipeline.com>
2023-04-17 16:00:48 -0600

    created navbar.js

---
+++ src/components/navigation/Navbar.js
@@ -0,0 +1,40 @@
+/*
+Navigation
+	- Sidebar or Header
+	- Footer
+	- Pages MAY have other links to navigate around the app as well (like a "go back" type button)
+*/
+
+import { NavLink } from "react-router-dom";
+
+export default function Navbar(props) {
+  return (
+    <div className="navbar">
+      <div className="nav-left">
+        <div className="home-nav-wrapper">
+          <NavLink exact to='/'>
+            Home
+          </NavLink>
+        </div>
+
+        <div className="about-nav-wrapper">
+          <NavLink exact to='/about'>
+            About
+          </NavLink>
+        </div>
+
+      </div>
+
+      <div className="nav-right">
+        <div className="search-bar">
+          <input
+            // value={props.SearchQuery}
+            // onChange={((event)=> props.setSearchQuery(event.target.value))}
+            type="text"
+            placeholder="Search for a show"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
