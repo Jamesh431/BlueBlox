524926b99c729444e8c6368fd7308ba880cafc90
james <james@devpipeline.com>
2023-04-17 15:08:43 -0600

    created format for landing page

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,13 @@
+/*
+- Contact
+		- Web form
+			- email
+			- first name
+			- last name
+			- subject
+			- content (text area)
+			- Submit button
+				- This needs a submit handler, but you just need to display something like an alert, it doesn't have to actually send an email off.
+		- Contact information
+			- Does not need to actually contain your real info. Fake info is perfect.
+*/
--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,19 +1,84 @@
 export default function Landing() {
   <div className="LandingPage">
     <div className="SortedShows">
-      <div className="Popular"></div>
-      <div className="Category"></div>
-      <div className="Category"></div>
-      <div className="Category"></div>
-      <div className="Category"></div>
-      <div className="Category"></div>
-      <div className="Category"></div>
-      <div className="Category"></div>
-      <div className="Category"></div>
-      <div className="Category"></div>
-      <div className="Category"></div>
-      <div className="Category"></div>
-      <div className="Category"></div>
+      
+      <div className="CategoryContainer" id="Popular">
+        <div className="HeaderWrapper">
+          <h1>Most Popular</h1>
+        </div>
+
+        <div className="ShowsWrapper">
+          <div className="ShowContainer">Insert Show Here</div>
+          <div className="ShowContainer">Insert Show Here</div>
+          <div className="ShowContainer">Insert Show Here</div>
+          <div className="ShowContainer">Insert Show Here</div>
+        </div>
+      </div>
+
+      <div className="CategoryContainer">
+        <div className="HeaderWrapper">
+          <h1>Category</h1>
+        </div>
+
+        <div className="ShowsWrapper">
+          <div className="ShowContainer">Insert Show Here</div>
+          <div className="ShowContainer">Insert Show Here</div>
+          <div className="ShowContainer">Insert Show Here</div>
+          <div className="ShowContainer">Insert Show Here</div>
+        </div>
+      </div>
+
+      <div className="CategoryContainer">
+        <div className="HeaderWrapper">
+          <h1>Category</h1>
+        </div>
+
+        <div className="ShowsWrapper">
+          <div className="ShowContainer">Insert Show Here</div>
+          <div className="ShowContainer">Insert Show Here</div>
+          <div className="ShowContainer">Insert Show Here</div>
+          <div className="ShowContainer">Insert Show Here</div>
+        </div>
+      </div>
+
+      <div className="CategoryContainer">
+        <div className="HeaderWrapper">
+          <h1>Category</h1>
+        </div>
+
+        <div className="ShowsWrapper">
+          <div className="ShowContainer">Insert Show Here</div>
+          <div className="ShowContainer">Insert Show Here</div>
+          <div className="ShowContainer">Insert Show Here</div>
+          <div className="ShowContainer">Insert Show Here</div>
+        </div>
+      </div>
+
+      <div className="CategoryContainer">
+        <div className="HeaderWrapper">
+          <h1>Category</h1>
+        </div>
+
+        <div className="ShowsWrapper">
+          <div className="ShowContainer">Insert Show Here</div>
+          <div className="ShowContainer">Insert Show Here</div>
+          <div className="ShowContainer">Insert Show Here</div>
+          <div className="ShowContainer">Insert Show Here</div>
+        </div>
+      </div>
+
+      <div className="CategoryContainer">
+        <div className="HeaderWrapper">
+          <h1>Category</h1>
+        </div>
+
+        <div className="ShowsWrapper">
+          <div className="ShowContainer">Insert Show Here</div>
+          <div className="ShowContainer">Insert Show Here</div>
+          <div className="ShowContainer">Insert Show Here</div>
+          <div className="ShowContainer">Insert Show Here</div>
+        </div>
+      </div>
     </div>
   </div>
 }
