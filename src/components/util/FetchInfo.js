ec5d7966397d4472f5ee563a88574e39c392cc28
james <james@devpipeline.com>
2023-04-17 14:44:57 -0600

    attempts at sorting through fetch data

--- src/components/util/FetchInfo.js
+++ src/components/util/FetchInfo.js
@@ -1,23 +1,62 @@
 import { useState, useEffect } from "react";
 
+
+
 export default function FetchInfo() {
-  // const []
+  const showObjects = {}
+  // const [shows, setShows] = useState()
   
-  const tvMazeInfo = {
-    "username": "jdhales",
-    "apikey": "_nsfO4xzpJw0R-Vsg5GxsVEUeuu1ouFo"
-  }
+  // const tvMazeInfo = {
+  //   "username": "jdhales",
+  //   "apikey": "_nsfO4xzpJw0R-Vsg5GxsVEUeuu1ouFo"
+  // }
 
   useEffect(() => {
-    // fetch("https://api.tvmaze.com/singlesearch/shows?q=TheMandalorian")
-    // fetch("https://api.tvmaze.com/singlesearch/shows?q=The blacklist")
-    fetch("http://api.tvmaze.com/shows")
-      // method: "POST"
-      // body: JSON.stringify(tvMazeInfo),
-      // headers: { "Access-Control-Allow-Origin": "http://localhost:3000/" }
+    // fetch("https://api.tvmaze.com/singlesearch/shows?q=The-Mandalorian")
+    // fetch("https://api.tvmaze.com/singlesearch/shows?q=The-blacklist")
+    // method: "POST"
+    // body: JSON.stringify(tvMazeInfo),
+    // headers: { "Access-Control-Allow-Origin": "http://localhost:3000/" }
+    fetch("http://api.tvmaze.com/shows") 
     .then((res) => res.json())
-    .then ((data) => console.log(data))
+    .then ((data) => sortShows(data))
     .catch(err => console.log('Error: Fetch was not complete',err))
   })
+
+  function sortShows(ArrofObj) {
+    ArrofObj.sort((firstObj, secObj) => {
+      if (firstObj.rating > secObj.rating) {
+        return 1
+      } else {
+        return -1
+      }
+    })
+
+    console.log(ArrofObj)
+  }
+
+
+
+  // const [person, setPerson] = useState({})
+
+  // useEffect(() => {
+  //   const controller = new AbortController();
+  //   const { signal } = controller;
+
+  //   fetch("https://swapi.tech/api/people", {
+  //     signal
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log(data)
+  //     })
+  //     .catch((err) => {
+  //       console.error("Get Person Error: ", err);
+  //     });
+
+  //   return () => controller.abort();
+  // }, []);
 }
+
+
 
