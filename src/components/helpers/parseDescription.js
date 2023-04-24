6bcc4c77f64c05f870d038b148f6c87e6e373beb
james <james@devpipeline.com>
2023-04-24 16:06:31 -0600

    parsing show description succesfully revamped into parseDescription.js

--- src/components/helpers/parseDescription.js
+++ src/components/helpers/parseDescription.js
@@ -1,29 +1,22 @@
-let startingIndex = 0
 export default function ParseDescription(props) {
-  console.log(props.desc.length)
-  console.log(props.desc)
-  console.log(props.desc)
-  while (startingIndex < props.desc.length) {
-    const openingTag = props.desc.indexOf('<', 1)
-    const endingTag = props.desc.indexOf('>')
-
-    if (openingTag === -1) {
-      break;
+  let workingStr = ''
+  let doParse = false
+  
+  for (let i = 0; i < props.desc.length; i++) {
+    if (props.desc[i] === '<') {
+      doParse = false
+    } else if (props.desc[i] === '>') {
+      doParse = true
+    } else if (doParse) {
+      workingStr += props.desc[i]
     }
-    if (endingTag === -1) {
-      break;
-    }
-    let workingStr = props.desc.slice(0, openingTag) + props.desc.slice(endingTag + 1)
-    console.log(workingStr)
-    console.log(workingStr)
-    console.log(workingStr)
-    startingIndex = openingTag
-  } 
+  }
+  
+console.log(workingStr)
 
-
-  if (props.desc.length > props.num) {
-    return props.desc.slice(0, props.num) + '...'
+  if (workingStr.length > props.num) {
+    return workingStr.slice(0, props.num) + '...'
   } else {
-    return props.desc
+    return workingStr
   }
 }
