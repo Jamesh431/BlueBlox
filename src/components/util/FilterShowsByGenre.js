27b52bbcabee093e938711016c41d262d59e7976
james <james@devpipeline.com>
2023-06-08 11:24:57 -0600

    removed unused import

--- src/components/util/FilterShowsByGenre.js
+++ src/components/util/FilterShowsByGenre.js
@@ -1,16 +1,13 @@
-import { useEffect } from "react"
-
-import getShows from "../helpers/getShows.js"
+import getShows from "../helpers/getShows.js";
 
 export default async function FilterShowsByGenre(genreFilter) {
-  
   try {
     const showData = await getShows();
-    const parsedShows = showData.filter((show) => show.genres.includes(genreFilter));
+    const parsedShows = showData.filter((show) =>
+      show.genres.includes(genreFilter)
+    );
     return parsedShows;
   } catch (err) {
     console.error("SortByCategory Error: ", err);
   }
-  
 }
-
