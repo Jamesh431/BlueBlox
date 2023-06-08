0d48c7c2e714ae142f765a54c2d2952484fb39cd
james <james@devpipeline.com>
2023-06-08 11:32:04 -0600

    fixed warning that for sort expects a value to be returned at the end of arrow function

--- src/components/util/PaginatePopular.js
+++ src/components/util/PaginatePopular.js
@@ -1,46 +1,42 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import ScaleLoader from "react-spinners/ScaleLoader";
 
-import getShows from "../helpers/getShows.js"
-import PaginateShows from "./PaginateShows.js"
+import getShows from "../helpers/getShows.js";
+import PaginateShows from "./PaginateShows.js";
 
 export default function RenderPopular() {
-  const [shows, setShows] = useState([]) 
-  const [loaded, setLoaded] = useState(false)
-
+  const [shows, setShows] = useState([]);
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     getShows()
-     .then(data => {
-      const popluar = data.sort((firstObj, secObj) => {
-        if (firstObj.rating.average && secObj.rating.average) {
-          if (firstObj.rating.average > secObj.rating.average) {
-            return -1
-          } else {
-            return 1
+      .then((data) => {
+        const popluar = data.sort((firstObj, secObj) => {
+          if (firstObj.rating.average && secObj.rating.average) {
+            if (firstObj.rating.average > secObj.rating.average) {
+              return -1;
+            } else {
+              return 1;
+            }
           }
-        }
-    })
-      setLoaded(true)
-      setShows(popluar)
-    })
-     .catch(err => console.error("Get Shows Erro: ", err))
-  }, [])
+          return 0;
+        });
+        setLoaded(true);
+        setShows(popluar);
+      })
+      .catch((err) => console.error("Get Shows Erro: ", err));
+  }, []);
 
-    return(
-      loaded ? 
-          <PaginateShows category={'Top Rated'} sortedArrOfObj={shows}/>
-
-        :
-
-        <div className="loading"> 
-          <ScaleLoader
-          size={150}
-          aria-label="Loading Spinner"
-          data-testid="loader"
-          color= "#4BCFFA"
-          />
-        </div>
-    
-    ) 
-  }
+  return loaded ? (
+    <PaginateShows category={"Top Rated"} sortedArrOfObj={shows} />
+  ) : (
+    <div className="loading">
+      <ScaleLoader
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+        color="#4BCFFA"
+      />
+    </div>
+  );
+}
