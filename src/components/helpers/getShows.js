d1e36b9e542cc071514b850745fceb10a545ba0d
james <jdhales2686@gmail.com>
2023-09-05 17:22:06 -0600

    changed from non-secure to secure url

--- src/components/helpers/getShows.js
+++ src/components/helpers/getShows.js
@@ -1,9 +1,11 @@
-const getShows = async () =>  {
-  const shows = await fetch("http://api.tvmaze.com/shows") 
-    .then((res) => { return res.json() })
-    .catch(err => console.log('Error: Fetch was not complete',err))
+const getShows = async () => {
+  const shows = await fetch("https://api.tvmaze.com/shows")
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => console.log("Error: Fetch was not complete", err));
 
-  return shows
-}
+  return shows;
+};
 
 export default getShows;
